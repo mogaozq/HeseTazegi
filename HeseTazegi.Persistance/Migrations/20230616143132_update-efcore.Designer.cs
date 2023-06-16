7d4c8e7ca8f2be4a7903acb79875b0ec9dfa0950
Moga <mogaozq@gmail.com>
2023-06-16 18:02:57 +0330

    upgrade target framework from .net5 to .net6

--- HeseTazegi.Persistance/Migrations/20230616143132_update-efcore.Designer.cs
+++ HeseTazegi.Persistance/Migrations/20230616143132_update-efcore.Designer.cs
@@ -3,6 +3,7 @@ using HeseTazegi.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HeseTazegi.Persistance.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230616143132_update-efcore")]
+    partial class updateefcore
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
